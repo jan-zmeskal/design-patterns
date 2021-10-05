17ba38c72caa0e173a0eebd5ea9a5516bf59994c
Jan Zmeskal <janzmeskal@email.cz>
2021-10-05 09:56:07 +0200

    Final game using class-based strategies

--- strategy/game_final.py
+++ strategy/game_final.py
@@ -1,13 +1,61 @@
+from abc import ABC, abstractmethod
+
+
 def get_distance_to_enemy() -> int:
     """Just a mock for demo purposes."""
     return 1
 
 
+class AttackStrategy(ABC):
+    weapon_name = ""
+
+    @abstractmethod
+    def perform_attack(self, stats: dict) -> int:
+        """Determine attack power based on character's stats."""
+
+    def __str__(self) -> str:
+        return self.weapon_name
+
+
+class BranchAttackStrategy(AttackStrategy):
+    weapon_name = "branch"
+
+    def perform_attack(self, stats: dict) -> int:
+        if get_distance_to_enemy() <= 2:
+            return stats["strength"]
+        return 0
+
+
+class SwordAttackStrategy(AttackStrategy):
+    weapon_name = "sword"
+
+    def perform_attack(self, stats: dict) -> int:
+        if get_distance_to_enemy() <= 3:
+            strength = stats["strength"]
+            return strength * 4 if strength >= 10 else strength * 3
+        return 0
+
+
+class BowAttackStrategy(AttackStrategy):
+    weapon_name = "bow"
+
+    def perform_attack(self, stats: dict) -> int:
+        return stats["dexterity"] * 3
+
+
+class FireballAttackStrategy(AttackStrategy):
+    weapon_name = "fireball"
+
+    def perform_attack(self, stats: dict) -> int:
+        return stats["intelligence"] + stats["dexterity"] + 1
+
+
 class GameCharacter:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, attack_strategy: AttackStrategy = None) -> None:
         self.name = name
         self.level = 0
         self.strength = self.dexterity = self.intelligence = self.charisma = 5
+        self.attack_strategy = attack_strategy or BranchAttackStrategy()
 
     @property
     def stats(self) -> dict:
@@ -18,41 +66,26 @@ class GameCharacter:
             "charisma": self.charisma,
         }
 
-    def attack(self, mode: str) -> int:
-        # Check if valid attack mode was provided
-        if mode not in ["branch", "sword", "bow", "dagger"]:
-            raise ValueError(f"Unsupported attack mode: {mode}")
+    def attack(self) -> int:
+        power = self.attack_strategy.perform_attack(stats=self.stats)
 
-        # Compute power based on the attack mode
-        if mode == "branch":
-            if get_distance_to_enemy() <= 2:
-                power = self.strength
-            else:
-                power = 0
-        elif mode == "sword":
-            if get_distance_to_enemy() <= 3:
-                power = self.strength * 4 if self.strength >= 10 else self.strength * 3
-            else:
-                power = 0
-        elif mode == "bow":
-            power = self.dexterity * 3
-        elif mode == "dagger":
-            if get_distance_to_enemy() <= 1 and self.dexterity >= 7:
-                power = self.strength + self.dexterity
-            else:
-                power = 0
-
-        print(f"{self.name} attacks using {mode} dealing {power} damage")
+        print(
+            f"{self.name} attacks using {self.attack_strategy} dealing {power} damage"
+        )
 
         return power
+
+    def set_attack_strategy(self, attack_strategy: AttackStrategy) -> None:
+        self.attack_strategy = attack_strategy
 
 
 def main():
     arthas = GameCharacter(name="Arthas")
-    assert arthas.attack("branch") == 5
-    assert arthas.attack("sword") == 15
-    assert arthas.attack("bow") == 15
-    assert arthas.attack("dagger") == 0
+    assert arthas.attack() == 5
+    arthas.set_attack_strategy(BowAttackStrategy())
+    assert arthas.attack() == 15
+    arthas.set_attack_strategy(FireballAttackStrategy())
+    assert arthas.attack() == 11
 
 
 if __name__ == "__main__":
